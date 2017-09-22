5fdce0abe2578274dc4d441303e130678a79a94d
Naresh Kumar H <nareshkumarh@live.com>
2017-09-22 10:59:20 +0530

    Added Eureka Client

--- src/main/java/com/naresh/orderappmicroservice/OrderappMicroserviceApplication.java
+++ src/main/java/com/naresh/orderappmicroservice/OrderappMicroserviceApplication.java
@@ -2,6 +2,7 @@ package com.naresh.orderappmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableFeignClients
 @EnableOAuth2Client
+@EnableEurekaClient
 public class OrderappMicroserviceApplication {
 
 	public static void main(String[] args) {
